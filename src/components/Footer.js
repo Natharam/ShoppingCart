3f99f2fd4ec72515de0d9b862c5329342d14a510
Natharam <201651033@iiitvadodara.ac.in>
2020-11-11 10:45:45 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="footer-links">
+        <a
+          href="https://github.com/Natharam"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+        <span> / </span>
+        <a
+          href="https://twitter.com/Natharam"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+        <span> / </span>
+        <a
+          href="https://www.instagram.com/jaipal_natha_45_/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instagram
+        </a>
+        <span> / </span>
+        <a
+          href="mailto:contact@natharam.in"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Email Me
+        </a>
+        <span> / </span>
+        <a
+          href="https://nrj45.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Read My Blog
+        </a>
+      </p>
+      <p>
+        &copy; 2019 <strong>Veggy</strong> - Organic Green Store
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
